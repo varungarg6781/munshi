297f034bcb2875f3cc4930bb8659965fe8b44fb7
Varun Garg <varungarg@Varuns-MacBook-Air.local>
2024-03-11 01:30:43 +0530

    Working app init

--- app/layout.tsx
+++ app/layout.tsx
@@ -8,7 +8,7 @@ import { Toaster } from "react-hot-toast";
 // const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
-  title: "ChatPDF",
+  title: "Munshi",
 };
 
 export default function RootLayout({
